f55191ca74583ea21ecf6b44e8bfecf4a4dd60a6
kyleawayan <kyle@awayan.com>
2020-10-05 15:59:04 -0700

    get something working

---
+++ server.js
@@ -0,0 +1,12 @@
+const server = require("http").createServer();
+const io = require("socket.io")(server);
+io.on("connection", (client) => {
+  console.log("connect");
+  client.on("event", (data) => {
+    console.log(data);
+  });
+  client.on("disconnect", () => {
+    console.log("disconnected");
+  });
+});
+server.listen(8000);

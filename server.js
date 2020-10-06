123d90b3e20d410062903ae5a1038cdf176fbf84
kyleawayan <kyle@awayan.com>
2020-10-05 21:33:15 -0700

    create rooms

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const { Socket } = require("socket.io-client");
 const server = require("http").createServer();
 const io = require("socket.io")(server);
 io.on("connection", (client) => {
-  console.log("connect");
+  console.log("connected");
+  client.on("room", function (room) {
+    console.log(room);
+    client.join(room);
+    client.in(room).emit("event", "someone joined");
+  });
   client.on("event", (data) => {
     console.log(data);
   });
-  client.on("disconnect", () => {
-    console.log("disconnected");
-  });
+  client.on("disconnect", () => {});
 });
 server.listen(8000);

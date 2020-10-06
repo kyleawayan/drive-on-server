332f01df16907dbaffbf6107eeb58b1cb4364cd3
kyleawayan <kyle@awayan.com>
2020-10-05 17:36:48 -0700

    i dont know what im doing

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+const { Socket } = require("socket.io-client");
+
 const server = require("http").createServer();
 const io = require("socket.io")(server);
 io.on("connection", (client) => {
   console.log("connect");
+  setInterval(() => {
+    client.emit("event", "bruh");
+  }, 1000);
   client.on("event", (data) => {
     console.log(data);
   });

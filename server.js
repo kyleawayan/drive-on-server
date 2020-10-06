bbe67c374632aaf6075ef65dbc07b7037fb01c31
kyleawayan <kyle@awayan.com>
2020-10-06 00:21:53 -0700

    find distance

--- server.js
+++ server.js
@@ -1,22 +1,35 @@
-const { Socket } = require("socket.io-client");
-
 const server = require("http").createServer();
 const io = require("socket.io")(server);
+
+
 io.on("connection", (client) => {
   console.log("connected");
+
   client.on("room", function (room) {
     console.log(room);
     client.join(room);
     client.in(room).emit("event", "someone joined");
   });
+
   client.on("newuser", ({ room, username }) => {
     client.to(room).emit("newuser", username);
     console.log(room, username);
   });
+
   client.on("newlocation", ({ room, lat, lng }) => {
     client.to(room).emit("newlocation", { lat: lat, lng: lng });
     console.log(room, lat, lng);
   });
+
+  client.on("guesslocation", ({ room, username, distance }) => {
+    console.log(room, username, distance);;;;
+    client
+
+      .to(room)
+
+      .emit("results", { username: username, distance: distance });
+  });
+
   client.on("disconnect", () => {});
 });
 server.listen(8000);

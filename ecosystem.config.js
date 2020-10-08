2810bf09200506c2366bf2157169af332db86161
kyleawayan <kyle@awayan.com>
2020-10-07 17:40:45 -0700

    add pm2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      name: "drive-on-server",
+      script: "npm",
+      watch: "start",
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: "bitnami",
+      host: "driveonserver",
+      ref: "origin/master",
+      repo: "https://github.com/kyleawayan/drive-on-server",
+      path: "/home/bitnami/driveonserver",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "npm install && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+    },
+  },
+};

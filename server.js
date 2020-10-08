e292d78ad820d75d2dd9f517ce09c04b9f42da77
kyleawayan <kyle@awayan.com>
2020-10-07 21:45:38 -0700

    add server logs

--- server.js
+++ server.js
@@ -3,42 +3,43 @@ const io = require("socket.io")(server);
 
 
 io.on("connection", (client) => {
-  console.log("connected");
-
   client.on("room", function (room) {
+    console.log(`someone joined room ${room}`);;
     console.log(room);
     client.join(room);
     client.in(room).emit("event", "someone joined");
   });
 
   client.on("newuser", ({ room, username }) => {
+    console.log(`${username} joined room ${room}`);;
     client.to(room).emit("newuser", username);
     console.log(room, username);
   });
 
   client.on("startnewlocation", (room) => {
+    console.log(`${room} is looking for a location...`);;
     client.to(room).emit("startnewlocation", "starting");
   });
 
   client.on("newlocation", ({ room, lat, lng }) => {
+    console.log(`${room} is at ${lat} ${long}`);;
     client.to(room).emit("newlocation", { lat: lat, lng: lng });
     console.log(room, lat, lng);
   });
 
   client.on("guesslocation", ({ room, username, distance, guess }) => {
+    console.log(`${username} guessed ${guess} and was ${distance}mi off`);;
     console.log(room, username, distance, guess);
     client
 
       .to(room)
 
       .emit("results", {
-     
-          username: username,
-     
-          distance: distance,
-      
-         typedguess: guess,
-  
+        username: username,
+
+        distance: distance,
+
+        typedguess: guess,
       });
   });
 
